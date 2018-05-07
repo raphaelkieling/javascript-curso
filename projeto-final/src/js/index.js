e1c3d8db493fb6ded5f6b45a8e4a1b158ab0857f
Raphael Kieling <rapha_kieling@hotmail.com>
2018-05-07 13:33:52 -0300

    Projeto final

---
+++ projeto-final/src/js/index.js
@@ -0,0 +1,30 @@
+let teclas = Array.from(document.querySelectorAll('.tecla'));
+let keyboards = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+let soundsToEvent = [];
+
+const createAudioEvent = audio => audio.cloneNode();
+const addAudioEventList = audio => list => [...list, createAudioEvent(audio)];
+
+const playSound = sound => sound.play() && false;
+const timing = (ms, fn) => setTimeout(() => fn(), ms);
+
+// const removeClassInTiming = className => new Promise((resolvemreject)=>{
+
+// })
+
+const listeningClick = (tecla, index) => {
+    let audio = tecla.querySelector('audio');
+
+    keyboardJS.bind(keyboards[index], (e) => {
+        tecla.className += " clicado";
+        
+        timing(300, () => {
+            tecla.className = tecla.className.replace(' clicado', '');
+        });
+        
+        soundsToEvent = addAudioEventList(audio)(soundsToEvent).filter(playSound);
+
+    })
+}
+
+teclas.forEach(listeningClick)
